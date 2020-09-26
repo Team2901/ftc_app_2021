eedabc0e0f8c77dcb3f69f06544bce98926c72cb
dominice23507 <dominice23507@student.cghsnc.org>
2020-09-26 11:12:29 -0400

    I commented the code for the ProgrammingUltimateGoalHardware.java class.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/ProgrammingUltimateGoalHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/ProgrammingUltimateGoalHardware.java
@@ -4,15 +4,17 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class ProgrammingUltimateGoalHardware {
+    // Instance Variables
     public DcMotor leftMotor = null;
     public DcMotor rightMotor = null;
     public DcMotor middleMotor = null;
 
     public void init(HardwareMap hwMap){
+        // Define and Initialize Motors
         leftMotor = hwMap.dcMotor.get("left_drive");
         rightMotor = hwMap.dcMotor.get("right_drive");
         middleMotor = hwMap.dcMotor.get("middle_drive");
-
+        // Setting left motor to reverse, making the robot moveable now.
         leftMotor.setDirection(DcMotor.Direction.REVERSE);
 
     }
