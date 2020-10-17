718ef7027d8afa21625a9d96671f0285f300f2a6
dominice23507 <dominice23507@student.cghsnc.org>
2020-10-17 10:57:44 -0400

    I wrote a helper method to either have the wobble grabber close or open.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/ProgrammingUltimateGoalHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/ProgrammingUltimateGoalHardware.java
@@ -12,5 +12,18 @@ import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
 public class ProgrammingUltimateGoalHardware extends BaseUltimateGoalHardware {
-
+    /**
+     * This helper method configures the wobble goal grabber to either be opened or closed.
+     * @param isClosed determines whether we want the wobble grabber to close or open
+     */
+    public void configureWobbleGrabber(boolean isClosed){
+        // If true is passed as the parameter, the wobble grabber will close.
+        if(isClosed){
+            wobbleGrabber.setPosition(1);
+        }
+        // Otherwise, the wobble grabber will remain open.
+        else{
+            wobbleGrabber.setPosition(0);
+        }
+    }
 }
