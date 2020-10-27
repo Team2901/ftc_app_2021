5e68cb917f4d8af3400696a787edfbb3019e85a7
dominice23507 <dominice23507@student.cghsnc.org>
2020-10-27 17:31:25 -0400

    I commented the VuforiaUltimateGoalTest class.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UltimateGoal/TeleOp/VuforiaUltimateGoalTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UltimateGoal/TeleOp/VuforiaUltimateGoalTest.java
@@ -17,16 +17,21 @@ public class VuforiaUltimateGoalTest extends OpMode {
     public void init() {
         robot.init(this.hardwareMap);
         robot.initWebCamera(this.hardwareMap);
+
+        // Loading the Vuforia trackables.
         robot.webCamera.loadVuforiaTrackables("UltimateGoal");
+
+        // Saves blue tower trackable.
         VuforiaTrackable vuforiaBlueTower = robot.webCamera.vuforiaTrackables.get(0);
 
-        //This is used for telemetry purposes.
+        // This is used for telemetry purposes for identifying that the camera is seeing the blue tower trackable.
         vuforiaBlueTower.setName("Blue Tower");
 
-        //This props up the blue tower, it is currently in the middle of the field.
+        // This props up the blue tower, it is currently in the middle of the field.
         OpenGLMatrix blueTowerLocation = OpenGLMatrix.rotation(AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES, 90,0,-90);
         vuforiaBlueTower.setLocation(blueTowerLocation);
 
+        // Sets up the position of the Vuforia web image and web camera.
         OpenGLMatrix webcamLocation = OpenGLMatrix.rotation(AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES, 0,-90,0);
     }
 
