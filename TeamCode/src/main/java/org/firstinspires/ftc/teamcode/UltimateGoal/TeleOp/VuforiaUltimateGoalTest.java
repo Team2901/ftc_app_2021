d686416066be559e228422c5f59584969a9810a7
GranttheCreator <ozakig24229@student.cghsnc.org>
2020-10-27 16:57:45 -0400

    I propped up the blue tower goal.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UltimateGoal/TeleOp/VuforiaUltimateGoalTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UltimateGoal/TeleOp/VuforiaUltimateGoalTest.java
@@ -3,6 +3,11 @@ package org.firstinspires.ftc.teamcode.UltimateGoal.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
 import org.firstinspires.ftc.teamcode.UltimateGoal.Hardware.TankUltimateGoalHardware;
 
 @TeleOp(name = "Vuforia UltimateGoal Test", group = "2021_UltimateGoal")
@@ -11,6 +16,16 @@ public class VuforiaUltimateGoalTest extends OpMode {
     @Override
     public void init() {
         robot.init(this.hardwareMap);
+        robot.initWebCamera(this.hardwareMap);
+        robot.webCamera.loadVuforiaTrackables("UltimateGoal");
+        VuforiaTrackable vuforiaBlueTower = robot.webCamera.vuforiaTrackables.get(0);
+
+        //This is used for telemetry purposes.
+        vuforiaBlueTower.setName("Blue Tower");
+
+        //This props up the blue tower, it is currently in the middle of the field.
+        OpenGLMatrix blueTowerLocation = OpenGLMatrix.rotation(AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES, 90,0,-90);
+        vuforiaBlueTower.setLocation(blueTowerLocation);
     }
 
     @Override
