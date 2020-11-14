4bf368b0a447a877f649658c339533797e87224b
Margaret Toebes <mtoebes@republicwireless.com>
2020-11-14 01:34:26 +0000

    add helper method to try/get hardware devices

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UltimateGoal/Hardware/MtoebesUltimateGoalHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UltimateGoal/Hardware/MtoebesUltimateGoalHardware.java
@@ -6,10 +6,15 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.Utility.MissingDcMotor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MtoebesUltimateGoalHardware extends BaseUltimateGoalHardware {
     public DcMotor middleMotor = null;
 
     public DcMotor intakeMotor = null;
+
+    List<String> missingHardwareNames = new ArrayList<>();
 
     @Override
     public void init(HardwareMap hwMap) {
@@ -18,12 +23,21 @@ public class MtoebesUltimateGoalHardware extends BaseUltimateGoalHardware {
         middleMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         middleMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-        intakeMotor = hwMap.tryGet(DcMotor.class, "intake_motor");
-
-        if (intakeMotor == null) {
-            intakeMotor = new MissingDcMotor();
-        }
-
+        intakeMotor = getDevice(hwMap, DcMotor.class, "intake_motor");
         intakeMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    }
+
+    public <T> T getDevice(HardwareMap hwMap, Class<? extends T> classOrInterface, String motorName) {
+        try {
+            return hwMap.get(classOrInterface, motorName);
+        } catch (Exception e) {
+            missingHardwareNames.add(motorName);
+
+            if (classOrInterface == DcMotor.class) {
+                return classOrInterface.cast(new MissingDcMotor());
+            } else {
+                return null;
+            }
+        }
     }
 }
