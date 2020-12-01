e788c30310be417c3d95f8096fe11f7b73d22ece
dominice23507 <dominice23507@student.cghsnc.org>
2020-12-01 17:13:20 -0500

    I made it such that the robot turns to angle 0 to face the tower goal if it cannot see an image.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UltimateGoal/TeleOp/VuforiaUltimateGoalTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UltimateGoal/TeleOp/VuforiaUltimateGoalTest.java
@@ -105,6 +105,8 @@ public class VuforiaUltimateGoalTest extends OpMode {
             // Gets the robot's location.
             OpenGLMatrix robotLocation = currentTrackableDefaultListener.getRobotLocation();
 
+            double relativeFieldAngle;
+
             /*
             * If the robot location is not null, we translate the robot's location. In other words,
             * if the tracker image is visible, we translate the robot's location.
@@ -120,7 +122,11 @@ public class VuforiaUltimateGoalTest extends OpMode {
                 double relativeRobotAngle = Math.toDegrees(Math.atan(y/x));
 
                 // Calculate angle relative to the field.
-                double relativeFieldAngle = relativeRobotAngle + robot.getAngle();
+                relativeFieldAngle = relativeRobotAngle + robot.getAngle();
+
+                // Print angle relative to the robot and the angle relative to the field.
+                telemetry.addData("Angle relative to the robot", relativeRobotAngle);
+                telemetry.addData("Angle relative to the field", relativeFieldAngle);
 
                 // This gets what this trackable thinks that the robot's orientation is.
                 Orientation orientation = Orientation.getOrientation(robotLocation, AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES);
@@ -133,10 +139,11 @@ public class VuforiaUltimateGoalTest extends OpMode {
 
                 // x, y, z rotations (x, y, z)
                 telemetry.addData("x, y, z rotations", String.format("(%f, %f, %f)", xAngle, yAngle, zAngle));
-
-                // Print angle relative to the robot and the angle relative to the field.
-                telemetry.addData("Angle relative to the robot", relativeRobotAngle);
-                telemetry.addData("Angle relative to the field", relativeFieldAngle);
+            }
+            else
+            {
+                // We want to face the tower goal, so set the angle to 0.
+                relativeFieldAngle = 0;
             }
         }
 
