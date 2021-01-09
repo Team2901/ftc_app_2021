c1ab2d9ae5abfd2647326dd23d9e963b0cca99e7
meyersw23562 <55461160+meyersw23562@users.noreply.github.com>
2021-01-09 11:47:57 -0500

    Added B routine to ProperUltimateGoalAuto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UltimateGoal/Autonomous/ProperUltimateGoalAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UltimateGoal/Autonomous/ProperUltimateGoalAuto.java
@@ -97,6 +97,31 @@ public class ProperUltimateGoalAuto extends BaseUltimateGoalAuto {
             while(!gamepad1.a && opModeIsActive()){}
             goToA();
         } else if (starterStackResult == 1) {
+            while(!gamepad1.a && opModeIsActive()){}
+            goToB();
+            moveInchesForward(-24, false);
+            while(!gamepad1.a && opModeIsActive()){}
+            robot.intakeMotor.setPower(.5);
+            robot.intakeMotor.setPower(-.5);
+            moveInchesForward(-15, false);
+            moveInchesForward(3, false);
+            robot.intakeMotor.setPower(0);
+            robot.intakeMotor.setPower(0);
+            while(!gamepad1.a && opModeIsActive()){}
+            moveInchesCenter(22);
+            moveInchesForward(24, true);
+            while(!gamepad1.a && opModeIsActive()){}
+            ringShot(1);
+            moveInchesCenter(-34);
+            while(!gamepad1.a && opModeIsActive()){}
+            moveInchesForward(-42, false);
+            turnToDesiredAngle(90);
+            moveInchesCenter(-6);
+            grabWobble();
+            while(!gamepad1.a && opModeIsActive()){}
+            turnToDesiredAngle(0);
+            moveInchesForward(48, true);
+            while(!gamepad1.a && opModeIsActive()){}
             goToB();
         } else if (starterStackResult == 2) {
             goToC();
