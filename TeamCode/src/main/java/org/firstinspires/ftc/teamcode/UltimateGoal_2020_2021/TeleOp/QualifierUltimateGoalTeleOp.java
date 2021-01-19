b7608c772b004197d6f0229e0c1d3e71a087e77e
setiai23785 <54600852+setiai23785@users.noreply.github.com>
2021-01-19 17:22:11 -0500

    Added kicker servo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UltimateGoal_2020_2021/TeleOp/QualifierUltimateGoalTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UltimateGoal_2020_2021/TeleOp/QualifierUltimateGoalTeleOp.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.internal.android.dx.rop.cst.CstArray;
 import org.firstinspires.ftc.teamcode.Shared.Gamepad.ImprovedGamepad;
 import org.firstinspires.ftc.teamcode.UltimateGoal_2020_2021.Hardware.BaseUltimateGoalHardware;
 import org.firstinspires.ftc.teamcode.UltimateGoal_2020_2021.Hardware.QualifierUltimateGoalHardware;
