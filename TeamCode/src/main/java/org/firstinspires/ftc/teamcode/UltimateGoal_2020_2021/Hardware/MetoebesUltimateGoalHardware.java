4d634989aaaae106ddaeed5461a3142f9d176ba7
Mary Ellen Toebes <maryellen@toebes.com>
2021-01-23 12:54:58 -0500

    Get the name of the active robot configuration

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UltimateGoal_2020_2021/Hardware/MetoebesUltimateGoalHardware.java
@@ -0,0 +1,39 @@
+package org.firstinspires.ftc.teamcode.UltimateGoal_2020_2021.Hardware;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+
+public class MetoebesUltimateGoalHardware extends BaseUltimateGoalHardware {
+    // gobilda 5202-0002-0019  yellow jacket - 312 RPM = 1 rev of 4" wheels =  12.6" in  .19sec
+    public static final double TICKS_PER_MOTOR_REV = 537.6;
+    public static final double FORWARD_DRIVE_GEAR_RATIO = 1.0;
+    public static final double RECOMMENDED_MAX_POWER = .3;
+    public static final double RECOMMENDED_MIN_POWER = .3;
+
+    public static final double CENTER_DRIVE_GEAR_RATIO = 1;
+    public static final double FORWARD_TICKS_PER_DRIVE_REV = TICKS_PER_MOTOR_REV * FORWARD_DRIVE_GEAR_RATIO;
+    public static final double CENTER_TICKS_PER_DRIVE_REV = TICKS_PER_MOTOR_REV * CENTER_DRIVE_GEAR_RATIO;
+    public static final double WHEEL_CIRCUMFERENCE_INCHES = 4 * Math.PI;
+    public static final double FORWARD_TICKS_PER_INCH = FORWARD_TICKS_PER_DRIVE_REV / WHEEL_CIRCUMFERENCE_INCHES;
+    public static final double CENTER_TICKS_PER_INCH = CENTER_TICKS_PER_DRIVE_REV / WHEEL_CIRCUMFERENCE_INCHES;
+
+    public MetoebesUltimateGoalHardware() {
+        super(FORWARD_TICKS_PER_INCH, CENTER_TICKS_PER_INCH);
+    }
+
+    @Override
+    public void init(HardwareMap hwMap) {
+
+        super.init(hwMap);
+
+        leftMotor.setDirection(DcMotor.Direction.REVERSE);
+        rightMotor.setDirection(DcMotor.Direction.FORWARD);
+
+        middleMotor.setDirection(DcMotor.Direction.REVERSE);
+    }
+
+    public double getRecommendedMaxMotorSpeed() {
+        return RECOMMENDED_MAX_POWER;
+    }
+}
