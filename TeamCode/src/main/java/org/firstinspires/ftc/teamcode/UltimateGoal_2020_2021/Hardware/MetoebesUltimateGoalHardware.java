e02bdfcbc50d4ce22efe95368640f0be021443c9
NiknutMusic <54417256+NiknutMusic@users.noreply.github.com>
2021-01-30 11:45:23 -0500

    Made FEET_PER_SECOND give us desired speed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UltimateGoal_2020_2021/Hardware/MetoebesUltimateGoalHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UltimateGoal_2020_2021/Hardware/MetoebesUltimateGoalHardware.java
@@ -10,6 +10,8 @@ public class MetoebesUltimateGoalHardware extends BaseUltimateGoalHardware {
     public static final double FORWARD_DRIVE_GEAR_RATIO = 1.0;
     public static final double RECOMMENDED_MAX_POWER = .3;
     public static final double RECOMMENDED_MIN_POWER = .3;
+    public static final double REVOLUTIONS_PER_MINUTE = 312;
+    public static final double REVOLUTIONS_PER_SECOND = REVOLUTIONS_PER_MINUTE / 60.0;
 
     public static final double CENTER_DRIVE_GEAR_RATIO = 1;
     public static final double FORWARD_TICKS_PER_DRIVE_REV = TICKS_PER_MOTOR_REV * FORWARD_DRIVE_GEAR_RATIO;
@@ -17,6 +19,8 @@ public class MetoebesUltimateGoalHardware extends BaseUltimateGoalHardware {
     public static final double WHEEL_CIRCUMFERENCE_INCHES = 4 * Math.PI;
     public static final double FORWARD_TICKS_PER_INCH = FORWARD_TICKS_PER_DRIVE_REV / WHEEL_CIRCUMFERENCE_INCHES;
     public static final double CENTER_TICKS_PER_INCH = CENTER_TICKS_PER_DRIVE_REV / WHEEL_CIRCUMFERENCE_INCHES;
+    public static final double FEET_PER_SECOND = REVOLUTIONS_PER_SECOND * WHEEL_CIRCUMFERENCE_INCHES;
+
 
     public MetoebesUltimateGoalHardware() {
         super(FORWARD_TICKS_PER_INCH, CENTER_TICKS_PER_INCH, 1);
@@ -35,5 +39,11 @@ public class MetoebesUltimateGoalHardware extends BaseUltimateGoalHardware {
 
     public double getRecommendedMaxMotorSpeed() {
         return RECOMMENDED_MAX_POWER;
+    }
+
+    public double getSpeed(double desiredFeetPerSecond){
+        //1 = FEET_PER_SECOND, x=desiredFeetPerSecond
+        double speed = desiredFeetPerSecond / FEET_PER_SECOND;
+        return speed;
     }
 }
