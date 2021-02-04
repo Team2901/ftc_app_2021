3299efb7df72cfb5b120e8a518a800fbff385c33
setiai23785 <54600852+setiai23785@users.noreply.github.com>
2021-02-04 17:33:45 -0500

    Added a teleop of auto aiming at power shot

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UltimateGoal_2020_2021/TeleOp/PowerShotAutoAimTeleOp.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode.UltimateGoal_2020_2021.TeleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "Power Shot Auto Aim", group = "2021_UltimateGoal")
+public class PowerShotAutoAimTeleOp extends OpMode {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
