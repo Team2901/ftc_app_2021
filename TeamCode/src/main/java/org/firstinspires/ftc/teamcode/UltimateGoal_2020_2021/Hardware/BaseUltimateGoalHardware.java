5a1b06624a2a6dda5fe098dd885cd56facb38a32
meyersw23562 <55461160+meyersw23562@users.noreply.github.com>
2021-02-06 12:33:13 -0500

    lets see if that works

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UltimateGoal_2020_2021/Hardware/BaseUltimateGoalHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UltimateGoal_2020_2021/Hardware/BaseUltimateGoalHardware.java
@@ -196,7 +196,7 @@ public class BaseUltimateGoalHardware {
     public void configureWobbleGrabber(boolean isClosed){
         // If true is passed as the parameter, the wobble grabber will close.
         if(isClosed){
-            wobbleGrabber.setPosition(.5);
+            wobbleGrabber.setPosition(.75);
         }
         // Otherwise, the wobble grabber will remain open.
         else{
