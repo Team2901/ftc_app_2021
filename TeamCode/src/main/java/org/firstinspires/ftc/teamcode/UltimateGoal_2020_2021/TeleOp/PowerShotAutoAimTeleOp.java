381df6161c6a2d3279985a27d6d638f5bb0ad547
setiai23785 <54600852+setiai23785@users.noreply.github.com>
2021-02-18 16:48:24 -0500

    fixed crservo in base ultimate hardware

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UltimateGoal_2020_2021/TeleOp/PowerShotAutoAimTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UltimateGoal_2020_2021/TeleOp/PowerShotAutoAimTeleOp.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp(name = "Power Shot Auto Aim", group = "2021_UltimateGoal")
-public class PowerShotAutoAimTeleOp extends OpMode {
+public class PowerShotAutoAimTeleOp extends KevinQualifierUltimateGoalTeleOp {
     @Override
     public void init() {
 
@@ -12,6 +12,16 @@ public class PowerShotAutoAimTeleOp extends OpMode {
 
     @Override
     public void loop() {
+        //use auto aim code from KevinUltimateGoalTeleOp
 
+        //once facing correct angle, stop auto aim
+
+        //use x and y distances from the image in atan(x/y) to get angle
+
+        //move in direction of robot angle +/- previous angle for distance between image and power shot
+
+        //pause and shoot
+
+        //move to next power shot
     }
 }
