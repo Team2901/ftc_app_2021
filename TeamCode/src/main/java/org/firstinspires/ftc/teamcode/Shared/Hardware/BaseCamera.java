6891a640afb5ff9da3b6e408708b7a8d29dfe649
meyersw23562 <55461160+meyersw23562@users.noreply.github.com>
2021-02-23 18:48:59 -0500

    did the clipping regions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Shared/Hardware/BaseCamera.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Shared/Hardware/BaseCamera.java
@@ -108,7 +108,7 @@ public class BaseCamera {
             tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
             tfod.loadModelFromAsset(assetName, labels);
             //tfod.setZoom(2, 1.7);
-            //tfod.setClippingMargins(0, 200, 200, 0);
+            tfod.setClippingMargins(0, 200, 0, 0);
         }
 
         return errorMessage;
