a57e58c568cb563c0f2c26f83d79ecc56f7d0b41
GranttheCreator <ozakig24229@student.cghsnc.org>
2021-03-23 16:42:09 -0400

    Renamed test gamepad classes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Shared/TeleOp/TestGamepad.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Shared/TeleOp/TestGamepad.java
@@ -3,43 +3,69 @@ package org.firstinspires.ftc.teamcode.Shared.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.util.ElapsedTime;
-
-import org.firstinspires.ftc.teamcode.Shared.Gamepad.ImprovedGamepad;
 
 @Disabled
 @TeleOp(name = "Gamepad Test", group = "Shared Test")
 public class TestGamepad extends OpMode {
 
-    public ElapsedTime timer = new ElapsedTime();
-    public ImprovedGamepad improvedGamepad1;
-    public ImprovedGamepad improvedGamepad2;
-
     @Override
     public void init() {
-        this.improvedGamepad1 = new ImprovedGamepad(gamepad1, timer, "g1");
-        this.improvedGamepad2 = new ImprovedGamepad(gamepad2, timer, "g2");
     }
 
     @Override
     public void loop() {
-        improvedGamepad1.update();
-        improvedGamepad2.update();
-
-        telemetry.addData("Pressed", improvedGamepad1.left_stick_y.isPressed());
-        telemetry.addData("Pressed Count", improvedGamepad1.left_stick.y.getPressedCounts());
-        telemetry.addData("Pressed Time", improvedGamepad1.left_stick.y.getPressedElapseTime());
-
-        telemetry.addData("Raw x", improvedGamepad1.left_stick.x.getRawValue());
-        telemetry.addData("Raw y", improvedGamepad1.left_stick.y.getRawValue());
-        telemetry.addData("x", improvedGamepad1.left_stick.x.getValue());
-        telemetry.addData("y", improvedGamepad1.left_stick.y.getValue());
-
-        telemetry.addData("Raw Radius", improvedGamepad1.left_stick.getRawValue());
-        telemetry.addData("Radius", improvedGamepad1.left_stick.getValue());
-        telemetry.addData("Raw Angle", improvedGamepad1.left_stick.getRawAngle());
-        telemetry.addData("Angle", improvedGamepad1.left_stick.getAngel());
-
+        /*
+        Is this button pressed? If this button is pressed buttonBlink is pressed. The end goal
+        is to print that the button has been pressed (this is to be done for one controller).
+         */
+        if (gamepad1.a) {
+            telemetry.addLine("button a is pressed");
+        }
+        if (gamepad1.b) {
+            telemetry.addLine("button b is pressed");
+        }
+        if (gamepad1.x) {
+            telemetry.addLine("button x is pressed");
+        }
+        if (gamepad1.y) {
+            telemetry.addLine("button y is pressed");
+        }
+        if (gamepad1.left_bumper) {
+            telemetry.addLine("left bumper is pressed");
+        }
+        if (gamepad1.right_bumper) {
+            telemetry.addLine("right bumper is pressed");
+        }
+        if (gamepad1.left_trigger > .25) {
+            telemetry.addLine("left trigger is pressed");
+        }
+        if (gamepad1.right_trigger > .25) {
+            telemetry.addLine("right trigger is pressed");
+        }
+        if (gamepad1.left_stick_x > 0) {
+            telemetry.addLine("left joystick is pointed right");
+        }
+        if (gamepad1.left_stick_x < 0) {
+            telemetry.addLine("left joystick is pointed left");
+        }
+        if (gamepad1.right_stick_x > 0) {
+            telemetry.addLine("right joystick is pointed right");
+        }
+        if (gamepad1.right_stick_x < 0) {
+            telemetry.addLine("right joystick is pointed left");
+        }
+        if (gamepad1.left_stick_y > 0) {
+            telemetry.addLine("left joystick is pointed down");
+        }
+        if (gamepad1.left_stick_y < 0) {
+            telemetry.addLine("left joystick is pointed up");
+        }
+        if (gamepad1.right_stick_y > 0) {
+            telemetry.addLine("right joystick is pointed down");
+        }
+        if (gamepad1.right_stick_y < 0) {
+            telemetry.addLine("right joystick is pointed up");
+        }
         telemetry.update();
     }
 }
