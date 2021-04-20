8f41e8b2fe8c268b416a8c45bf57699978808482
setiai23785 <54600852+setiai23785@users.noreply.github.com>
2021-04-20 16:16:24 -0400

    added telemetry to print out how many times a button is pressed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Shared/TeleOp/TestDDRPad.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Shared/TeleOp/TestDDRPad.java
@@ -40,42 +40,42 @@ public class TestDDRPad extends OpMode {
         telemetry.addData("Are buttons active", dancePad.areButtonsActive());
 
         if(dancePad.upArrow.getValue()){
-            telemetry.addData("Up Arrow Time Pressed", dancePad.upArrow.getPressedCounts());
+            telemetry.addData("Up Arrow Times Pressed", dancePad.upArrow.getPressedCounts());
             telemetry.addLine("Up arrow");
         }
 
         if(dancePad.downArrow.getValue()){
-            telemetry.addData("Down Arrow Time Pressed", dancePad.downArrow.getPressedCounts());
+            telemetry.addData("Down Arrow Times Pressed", dancePad.downArrow.getPressedCounts());
             telemetry.addLine("Down arrow");
         }
 
         if(dancePad.leftArrow.getValue()){
-            telemetry.addData("Left Arrow Time Pressed", dancePad.leftArrow.getPressedCounts());
+            telemetry.addData("Left Arrow Times Pressed", dancePad.leftArrow.getPressedCounts());
             telemetry.addLine("Left arrow");
         }
 
         if(dancePad.rightArrow.getValue()){
-            telemetry.addData("Right Arrow Time Pressed", dancePad.rightArrow.getPressedCounts());
+            telemetry.addData("Right Arrow Times Pressed", dancePad.rightArrow.getPressedCounts());
             telemetry.addLine("Right arrow");
         }
 
         if(dancePad.topLeftArrow.getValue()){
-            telemetry.addData("TopLeft Arrow Time Pressed", dancePad.topLeftArrow.getPressedCounts());
+            telemetry.addData("TopLeft Arrow Times Pressed", dancePad.topLeftArrow.getPressedCounts());
             telemetry.addLine("X");
         }
 
         if(dancePad.topRightArrow.getValue()){
-            telemetry.addData("TopRight Arrow Time Pressed", dancePad.topRightArrow.getPressedCounts());
+            telemetry.addData("TopRight Arrow Times Pressed", dancePad.topRightArrow.getPressedCounts());
             telemetry.addLine("O");
         }
 
         if(dancePad.startButton.getValue()){
-            telemetry.addData("Start Button Time Pressed", dancePad.startButton.getPressedCounts());
+            telemetry.addData("Start Button Times Pressed", dancePad.startButton.getPressedCounts());
             telemetry.addLine("Start button");
         }
 
         if(dancePad.a.getValue()){
-            telemetry.addData("A Time Pressed", dancePad.a.getPressedCounts());
+            telemetry.addData("A Times Pressed", dancePad.a.getPressedCounts());
             telemetry.addLine("a button");
         }
 
